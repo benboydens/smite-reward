a5af827f191e3467c676cde36a3ef39dff36ca35
benboydens <benboydens@gmail.com>
2024-01-11 00:22:17 +0100

    added logo + gitignore

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Smite reward script
+
+## TODO
+
+- let script wait for steam updates
+- detect if user started pc (to avoid anoying pop up when pc starts)
+- shut down computer when done
--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
             script="smreward.py", 
             base="console",
             target_name="smreward",
+            icon="logo.ico",
             shortcut_name="sm_reward_startup",
             shortcut_dir="StartupFolder"
         )
